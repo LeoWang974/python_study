88f02daf33a225253bbb84f7026218eaab14f8fb
LeoWang974 <wb990704@163.com>
2024-08-26 21:48:30 +0800

    python numpy basic code

---
+++ basicGram/pythonBasic/numpyStudy.py
@@ -0,0 +1,38 @@
+import re
+from numpy import *
+import pandas as pds
+
+"""
+parten0 = 'www'
+parten1 = 'com'
+string = 'www.baidu.com'
+print(re.match(parten0, string, flags=re.I))    #在起始位置匹配
+#匹配成功，返回匹配对象
+print(re.match(parten1, string))                #不在起始位置匹配
+#匹配失败，返回none
+
+
+#数组创建
+a = np.array([2, 3, 4])
+print('a=', a)
+print('type=', a.dtype)
+
+#创建数组对象时指定类型
+c = np.array([[1, 2],[3, 4]], dtype=complex) #注意，此处的数组为二维数组
+print('c=', c)
+
+x = np.zeros((3, 4))    #创建元素全为0的多维数组
+print('x=', x)
+y = np.ones((2, 3, 4), dtype=int) #创建全为1的多维数组
+print('y=', y)
+z = np.empty((3, 2), dtype=int)#创建元素全未初始化的多维数组
+print('z=', z)
+
+a = arange(10)  #生成一个长度为10的序列数组
+print('a=', a)
+print(a[3:6])   #对数组a进行切片操作
+"""
+
+
+
+
